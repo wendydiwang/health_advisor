ce5ed657942702fdf1477e5273c6c01d8f46d57c
wendydiwang <me@wendydiwang.com>
2017-08-26 15:58:31 -0700

    add jquery, add flask endpoints

---
+++ health_advisor/server.py
@@ -0,0 +1,28 @@
+import os, sys
+import userinfo
+from flask import Flask, render_template, request
+
+'''
+if getattr(sys, 'frozen', False):
+    template_folder = os.path.join(sys._MEIPASS, 'templates')
+    app = Flask(__name__, template_folder=template_folder)
+else:
+'''
+app = Flask(__name__, template_folder='/Users/mac/Desktop/health_advisor/health_advisor/templates')
+
+app.run(port=50000)
+
+@app.route('/', methods=['GET'])
+def show_index():
+    return render_template('userinfo.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+
+    user_info = userinfo.Userinfo(request.form['name'], request.form['gender'], request.form['age'],
+                        request.form['weight'], request.form['height'], request.form['objective'],
+                        request.form['preferred_time'], request.form['use_DNA_data'], request.form['use_wearable_data'],
+                        request.form['term'])
+    # upload userinfo to database
+    print(request.form)
+
---
+++ health_advisor/userinfo.py
@@ -0,0 +1,12 @@
+class Userinfo:
+    def __init__(self, name, gender, age, weight, height, objective, preferred_time, use_DNA_data, use_wearable_data, term):
+        self.name = name
+        self.gender = gender
+        self.age = age
+        self.weight = weight
+        self.height = height
+        self.objective = objective
+        self.preferred_time = preferred_time
+        self.use_DNA_data = use_DNA_data
+        self.use_wearable_data = use_wearable_data
+        self.term = term
